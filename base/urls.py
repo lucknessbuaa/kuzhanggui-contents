55f7ccdbf1a4dbe1aadda07d4d8ba0df63007b2f
jarvys <yuhan534@126.com>
2014-08-29 17:09:19 +0800

    modify api url

--- base/urls.py
+++ base/urls.py
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
     (r'^ckeditor/', include('ckeditor.urls')),
     url(r'^ajax-upload/', include('ajax_upload.urls')),
     url(r'^contents/backend/',include('contents.urls')),
-    url(r'^API/',include(v.urls)),
-    url(r'^API/likes$',like)
+    url(r'^contents/API/',include(v.urls)),
+    url(r'^contents/API/likes$',like)
 )
 if settings.DEBUG:
     urlpatterns += patterns('',
