ecd40df2fa8db563805678c06a75a723772069be
heqingyang <heqingyang@limijiaoyin.com>
2014-09-03 18:31:59 +0800

    save

--- static/js/codes.js
+++
@@ -1,7 +0,0 @@
-define(function() {
-    return {
-        ok: 0,
-        AuthFailure: 1001,
-        FormInvalid: 1002
-    };
-});
--- static/js/shake/token.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-    var token = localStorage.getItem("audience-token");
-    if (!token) {
-        token = new String(new Date().getTime());
-        localStorage.setItem("audience-token", token);
-    }
-
-    return token
-});
