2750b00f4c4ad5883a926de7a919ea86e9a87539
jarvys <yuhan534@126.com>
2014-09-04 16:53:39 +0800

    save

--- base/common.py
+++ base/common.py
@@ -29,7 +29,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'oauth2_provider.middleware.OAuth2TokenMiddleware',
 )
 
